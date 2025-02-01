a089a0de771e3e5797b6b3cbd4d5520fde441140
Navidad12 <elijahgabriel7104@gmail.com>
2025-02-01 16:53:06 +0800

    Initialize and Insert done.

--- src/main/java/com/mycompany/databasehandlerlab/DatabaseConnect.java
+++ src/main/java/com/mycompany/databasehandlerlab/DatabaseConnect.java
@@ -2,7 +2,7 @@ package com.mycompany.databasehandlerlab;
 
 import java.sql.*;
 
-public class DatabaseConnect {
+public class DatabaseConnect{
     private Connection conn;
 
     public DatabaseConnect(String database) {
@@ -15,23 +15,148 @@ public class DatabaseConnect {
         }
     }
 
-    public void initializeStudents(){
-        String qry = "DROP TABLE IF EXISTS Students\r\n" + //
-                        "CREATE TABLE Students (\r\n" + //
-                        "student_number TEXT NOT NULL,\r\n" + //
-                        "student_fname TEXT NOT NULL,\r\n" + //
-                        "student_mname TEXT,\r\n" + //
-                        "student_lname TEXT NOT NULL,\r\n" + //
-                        "student_sex TEXT NOT NULL,\r\n" + //
-                        "student_birth TEXT NOT NULL,\r\n" + //
-                        "student_start INTEGER NOT NULL,\r\n" + //
-                        "student_department TEXT NOT NULL,\r\n" + //
-                        "student_units INTEGER NOT NULL,\r\n" + //
-                        "student_address TEXT,\r\n" + //
-                        "CONSTRAINT Students_PK PRIMARY KEY (student_number)\r\n" + //
+    public void initializeStudents() throws SQLException{
+        String dropqry = "DROP TABLE IF EXISTS Students;";
+        String qry =    "CREATE TABLE Students (\r\n" + 
+                        "student_number TEXT NOT NULL,\r\n" + 
+                        "student_fname TEXT NOT NULL,\r\n" + 
+                        "student_mname TEXT,\r\n" + 
+                        "student_lname TEXT NOT NULL,\r\n" + 
+                        "student_sex TEXT NOT NULL,\r\n" + 
+                        "student_birth TEXT NOT NULL,\r\n" + 
+                        "student_start INTEGER NOT NULL,\r\n" + 
+                        "student_department TEXT NOT NULL,\r\n" + 
+                        "student_units INTEGER NOT NULL,\r\n" + 
+                        "student_address TEXT,\r\n" + 
+                        "CONSTRAINT Students_PK PRIMARY KEY (student_number)\r\n" + 
                         ");";
-                        System.out.println(qry);
+        PreparedStatement dropstmt = conn.prepareStatement(dropqry);
+        dropstmt.executeUpdate();
+        PreparedStatement pstmt = conn.prepareStatement(qry);
+        pstmt.executeUpdate();
     }
 
+    public Student getStudents(String student_id) throws SQLException {
+        String qry = "SELECT * FROM Students WHERE student_number = ?";
+        PreparedStatement pstmt = conn.prepareStatement(qry);
+        pstmt.setString(1, student_id);
+        ResultSet rs = pstmt.executeQuery();
+        if(rs.next()){
+            return new Student(rs.getString("student_number"), 
+                                rs.getString("student_fname"), 
+                                rs.getString("student_mname"),
+                                rs.getString("student_lname"),
+                                rs.getString("student_sex"),
+                                rs.getString("student_birth"),
+                                rs.getInt("student_start"),
+                                rs.getString("student_department"),
+                                rs.getInt("student_units"),
+                                rs.getString("student_address")
+                                );
+        }else{
+            return null;
+        }        
+    }
+    public Student getStudent(String student_Fname, String student_Mname, String student_Lname) throws SQLException{
+        String qry = "SELECT * FROM Students s\r\n" + 
+                        "WHERE s.student_fname = ?\r\n" +
+                        "AND s.student_mname = ?\r\n" + 
+                        "AND s.student_lname = ?";
+        PreparedStatement pstmt = conn.prepareStatement(qry);
+        pstmt.setString(1, student_Fname);
+        pstmt.setString(2, student_Mname);
+        pstmt.setString(3, student_Lname);
+        ResultSet rs = pstmt.executeQuery();
+        if(rs.next()){
+            return new Student(rs.getString("student_number"), 
+                                rs.getString("student_fname"), 
+                                rs.getString("student_mname"),
+                                rs.getString("student_lname"),
+                                rs.getString("student_sex"),
+                                rs.getString("student_birth"),
+                                rs.getInt("student_start"),
+                                rs.getString("student_department"),
+                                rs.getInt("student_units"),
+                                rs.getString("student_address")
+                                );
+        }else{
+            return null;
+        }        
+    }
+
+    public boolean insetStudent(Student newStudent) throws SQLException{
+        String qry = "INSERT INTO Students (\r\n" + //
+                        "student_number\r\n" + //
+                        ", student_fname\r\n" + //
+                        ", student_mname\r\n" + //
+                        ", student_lname\r\n" + //
+                        ", student_sex\r\n" + //
+                        ", student_birth\r\n" + //
+                        ", student_start\r\n" + //
+                        ", student_department\r\n" + //
+                        ", student_units\r\n" + //
+                        ", student_address\r\n" + //
+                        ") values (\r\n" + //
+                        "?\r\n" + //
+                        ", ?\r\n" + //
+                        ", ?\r\n" + //
+                        ", ?\r\n" + //
+                        ", ?\r\n" + //
+                        ", ?\r\n" + //
+                        ", ?\r\n" + //
+                        ", ?\r\n" + //
+                        ", ?\r\n" + //
+                        ", ?\r\n" + //
+                        ")";
+            PreparedStatement pstmt = conn.prepareStatement(qry);
+            pstmt.setString(1, newStudent.getStudentId()); pstmt.setString(2, newStudent.getFirstName());
+            pstmt.setString(3, newStudent.getMiddleName()); pstmt.setString(4, newStudent.getLastName());
+            pstmt.setString(5, newStudent.getSex()); pstmt.setString(6, newStudent.getBirthDate());
+            pstmt.setInt(7, newStudent.getStartYear()); pstmt.setString(8, newStudent.getDepartment());
+            pstmt.setInt(9, newStudent.getUnits()); pstmt.setString(10, newStudent.getAddress());
+            return pstmt.executeUpdate() > 0;
+
+    }
+    public static class Student {
+        private String student_id;
+        private String student_Fname;
+        private String student_Mname;
+        private String student_Lname;
+        private String student_sex;
+        private String student_birth;
+        private int student_start;
+        private String student_department;
+        private int student_units;
+        private String student_address;
+        
+        Student(String student_id, String student_Fname, String student_Mname, String student_Lname, String student_sex, String student_birth,
+                int student_start, String student_department, int student_units, String student_address) {
+                    this.student_id = student_id;
+                    this.student_Fname = student_Fname;
+                    this.student_Mname = student_Mname;
+                    this.student_Lname = student_Lname;
+                    this.student_sex = student_sex;
+                    this.student_birth = student_birth;
+                    this.student_start = student_start;
+                    this.student_units = student_units;
+                    this.student_address = student_address;
+                    this.student_department = student_department;
+        }
+
+        public String getStudentId() { return student_id; }
+        public String getFirstName() { return student_Fname; }
+        public String getMiddleName() { return student_Mname; }
+        public String getLastName() { return student_Lname; }
+        public String getSex() { return student_sex; }
+        public String getBirthDate() { return student_birth; }
+        public int getStartYear() { return student_start; }
+        public String getDepartment() { return student_department; }
+        public int getUnits() { return student_units; }
+        public String getAddress() { return student_address; }
+        
+    }
 
 }
+
+
+
--- src/main/java/com/mycompany/databasehandlerlab/DatabaseHandlerLab.java
+++ src/main/java/com/mycompany/databasehandlerlab/DatabaseHandlerLab.java
@@ -4,15 +4,18 @@
 
 package com.mycompany.databasehandlerlab;
 
+import java.sql.SQLException;
+
 /**
  *
  * @author Elijah
  */
 public class DatabaseHandlerLab {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException{
         DatabaseConnect data = new DatabaseConnect("students.db");
-
         data.initializeStudents();
+        DatabaseConnect.Student stud = new DatabaseConnect.Student("20293021", "Karl", "Sotto", "Mapagmahal", "M", "2001-01-01", 2024, "CICS", 26, "Novaliches");
+        data.insetStudent(stud);
     }
 }
